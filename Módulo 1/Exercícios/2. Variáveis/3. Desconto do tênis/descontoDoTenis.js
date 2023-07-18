f4af70ca5877fc83ee617095ce323b4faa074935
Jonathan Barros <132490863+JonathanBarr0s@users.noreply.github.com>
2023-07-17 23:31:09 -0300

    Att

---
+++ Módulo 1/Exercícios/2. Variáveis/3. Desconto do tênis/descontoDoTenis.js
@@ -0,0 +1,5 @@
+let   valorTotal
+let   meuDinheiro
+const valorDoDesconto = valorTotal - meuDinheiro
+
+console.log(`O desconto deve ser de ${valorDoDesconto * 100 / valorTotal}%.`)
---
+++ Módulo 1/Exercícios/2. Variáveis/4. Calcular juros compostos/calcularJurosCompostos.js
@@ -0,0 +1,10 @@
+// M: montante (o capital acrescido dos juros, ou seja, Capital + Juros)
+// C: capital (valor inicial de uma dívida, empréstimo ou investimento)
+// i: taxa fixa de juros (expressa em decimal na fórmula, ou seja, o valor da taxa dividido por 100)
+// t: período de tempo
+
+let c
+let i
+let t
+
+console.log(`O valor do montante será de R$ ${c * ( 1 + i ) ** t}.`);
