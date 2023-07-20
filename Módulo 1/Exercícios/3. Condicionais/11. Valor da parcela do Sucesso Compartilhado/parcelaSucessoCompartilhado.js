22954eed3e596504c1b210c0d8ac25ac76a3a585
Jonathan Barros <132490863+JonathanBarr0s@users.noreply.github.com>
2023-07-20 11:35:28 -0300

    Att

--- Módulo 1/Exercícios/3. Condicionais/11. Valor da parcela do Sucesso Compartilhado/parcelaSucessoCompartilhado.js
+++ Módulo 1/Exercícios/3. Condicionais/11. Valor da parcela do Sucesso Compartilhado/parcelaSucessoCompartilhado.js
@@ -1,4 +1,4 @@
-const rendaMensalEmCentavos = 400000;
+const rendaMensalEmCentavos = 300000;
 
 // Tempo decorrido de contrato. Se for maior que 60 meses, o aluno não deve mais nada.
 const mesesDecorridos = 60;
@@ -8,16 +8,16 @@ Soma das parcelas já pagas pelo aluno nos meses anteriores (em centavos).
 Se for igual a 18 mil reais, o aluno não deve pagar mais nada,
 pois já quitou a dívida.
 */
-const totalJaPagoPeloAluno = 1700000;
+const totalJaPagoPeloAluno = 1800000;
 
 const valorParcela = (rendaMensalEmCentavos * 0.18) / 100
 
-if (
-    (rendaMensalEmCentavos <= 200000) ||
-    (mesesDecorridos > 60) ||
-    (totalJaPagoPeloAluno >= 1800000)
-) {
-    console.log("O valor da parcela desse mês é de R$ 0 reais.");
+if (rendaMensalEmCentavos < 200000) {
+    console.log("O valor da parcela desse mês é R$ 0 reais. Nenhum valor é devido pois a renda do estudante está abaixo do valor mínimo de R$ 2000 reais.");
+} else if (mesesDecorridos > 60) {
+    console.log("O valor da parcela desse mês é R$ 0 reais. Nenhum valor é devido pois o prazo de 60 meses para quitar a dívida venceu.");
+} else if (totalJaPagoPeloAluno >= 1800000) {
+    console.log("O valor da parcela desse mês é R$ 0 reais. Nenhum valor é devido pois a dívida foi quitada.");
 } else {
     console.log(`O valor da parcela desse mês é de R$ ${valorParcela} reais.`);
 }
