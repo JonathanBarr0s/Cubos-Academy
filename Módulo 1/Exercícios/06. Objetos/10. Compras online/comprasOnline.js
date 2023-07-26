6823105ced85984f1d9a9193f3bc21435f3e0f5b
Jonathan Barros <132490863+JonathanBarr0s@users.noreply.github.com>
2023-07-26 20:25:51 -0300

    Att.

--- Módulo 1/Exercícios/06. Objetos/10. Compras online/comprasOnline.js
+++ Módulo 1/Exercícios/06. Objetos/10. Compras online/comprasOnline.js
@@ -1,11 +1,10 @@
-/*
 const carrinho = {
     item: {
         nome: 'TV Samsung 4K',
         valorEmCentavos: 129900
     },
     quantidade: 1
-} */
+}
 
 // clientes
 const patricia = { nome: "Patricia", carrinho: [] }
@@ -24,4 +23,40 @@ const carregador = { nome: "Carregador portátil", valorEmCentavos: 4590 }
 const webcam = { nome: "Webcam C920s", valorEmCentavos: 80000 }
 const monitor = { nome: "Monitor LG 29 FHD", valorEmCentavos: 129900 }
 
-for (let i = 0; i < 
+jose.carrinho.push(
+    { item: { nome: tv.nome, valorEmCentavos: tv.valorEmCentavos }, quantidade: 1 },
+    { item: { nome: caboUsb.nome, valorEmCentavos: caboUsb.valorEmCentavos }, quantidade: 2 },
+    { item: { nome: webcam.nome, valorEmCentavos: webcam.valorEmCentavos }, quantidade: 2 }
+);
+
+console.log(jose);
+
+carlos.carrinho.push(
+    { item: { nome: notebook.nome, valorEmCentavos: notebook.valorEmCentavos }, quantidade: 2 }
+);
+
+console.log(carlos);
+
+patricia.carrinho.push(
+    { item: { nome: teclado.nome, valorEmCentavos: teclado.valorEmCentavos }, quantidade: 1 },
+    { item: { nome: caboUsb.nome, valorEmCentavos: caboUsb.valorEmCentavos }, quantidade: 2 },
+    { item: { nome: carregador.nome, valorEmCentavos: carregador.valorEmCentavos }, quantidade: 1 },
+    { item: { nome: mouse.nome, valorEmCentavos: mouse.valorEmCentavos }, quantidade: 1 },
+    { item: { nome: monitor.nome, valorEmCentavos: monitor.valorEmCentavos }, quantidade: 1 }
+);
+
+console.log(patricia);
+
+renato.carrinho.push(
+    { item: { nome: webcam.nome, valorEmCentavos: webcam.valorEmCentavos }, quantidade: 5 }
+);
+
+console.log(renato);
+
+roberto.carrinho.push(
+    { item: { nome: webcam.nome, valorEmCentavos: webcam.valorEmCentavos }, quantidade: 1 },
+    { item: { nome: caboUsb.nome, valorEmCentavos: caboUsb.valorEmCentavos }, quantidade: 2 },
+    { item: { nome: monitor.nome, valorEmCentavos: monitor.valorEmCentavos }, quantidade: 1 }
+);
+
+console.log(roberto);
