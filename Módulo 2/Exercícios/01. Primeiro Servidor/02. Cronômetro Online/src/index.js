a1900af7dbaf3b4d4b2729edc60a729d91f8084c
Jonathan Barros <132490863+JonathanBarr0s@users.noreply.github.com>
2023-08-22 23:15:45 -0300

    Att

--- Módulo 2/Exercícios/01. Primeiro Servidor/02. Cronômetro Online/src/index.js
+++ Módulo 2/Exercícios/01. Primeiro Servidor/02. Cronômetro Online/src/index.js
@@ -2,26 +2,41 @@ const express = require("express");
 const app = express();
 
 let min = 0;
-let seg = 55;
+let seg = 0;
+
+let tempoAtual = "Tempo atual do cronômetro: 00 minutos e 00 segundos";
+let intervalId;
 
 function cronometro() {
+  seg++;
   if (seg === 60) {
     min++;
     seg = 0;
   }
-  const tempoAtual = `Tempo atual do cronômetro: ${min.toString().padStart(2, '0')} minutos e ${seg.toString().padStart(2, '0')} segundos`;
-  setTimeout(() => {
-    seg++;
-    cronometro();
-  }, 1000);
-  return tempoAtual;
+  tempoAtual = `Tempo atual do cronômetro: ${min
+    .toString()
+    .padStart(2, "0")} minutos e ${seg.toString().padStart(2, "0")} segundos`;
 }
 
-
 app.get("/", (req, res) => {
-    cronometro();
-  const tempoAtual = cronometro(); 
   res.send(tempoAtual);
+});
+
+app.get("/iniciar", (req, res) => {
+  intervalId = setInterval(cronometro, 1000);
+  res.send("Cronômetro iniciado!");
+});
+
+app.get("/pausar", (req, res) => {
+  clearInterval(intervalId);
+  intervalId = null;
+  res.send("Cronômetro pausado!");
+});
+
+app.get("/zerar", (req, res) => {
+  min = 0;
+  seg = 0;
+  res.send("Cronômetro zerado!");
 });
 
 app.listen(8000, () => {
