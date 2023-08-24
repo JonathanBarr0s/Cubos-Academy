7eba341a77a290ca5ba59c48b31f758730b572fa
Jonathan Barros <132490863+JonathanBarr0s@users.noreply.github.com>
2023-08-23 21:41:55 -0300

    Att

--- Módulo 2/Exercícios/02. Rotas, Intermediarios e Controladores/01. Calculadora Online/src/index.js
+++ Módulo 2/Exercícios/02. Rotas, Intermediarios e Controladores/01. Calculadora Online/src/index.js
@@ -2,15 +2,45 @@ const express = require("express");
 const app = express();
 const porta = 3000;
 
-let num1;
-let num2;
 let resultado;
 
 app.get("/somar", (req, res) => {
-  resultado = num1 + num2;
-  res.send(resultado);
+  const { num1: parametroNum1, num2: parametroNum2 } = req.query;
+
+  if (parametroNum1 && parametroNum2) {
+    resultado = Number(parametroNum1) + Number(parametroNum2);
+    res.send(resultado.toString());
+  }
 });
 
-app.listen(porta, () => {
-  console.log("Servidor iniciado.");
+app.get("/subtrair", (req, res) => {
+  const { num1: parametroNum1, num2: parametroNum2 } = req.query;
+
+  if (parametroNum1 && parametroNum2) {
+    resultado = Number(parametroNum1) - Number(parametroNum2);
+    res.send(resultado.toString());
+  }
 });
+
+app.get("/multiplicar", (req, res) => {
+  const { num1: parametroNum1, num2: parametroNum2 } = req.query;
+
+  if (parametroNum1 && parametroNum2) {
+    resultado = Number(parametroNum1) * Number(parametroNum2);
+    res.send(resultado.toString());
+  }
+});
+
+app.get("/dividir", (req, res) => {
+  const { num1: parametroNum1, num2: parametroNum2 } = req.query;
+
+  if (parametroNum1 && parametroNum2 && parametroNum2 !== "0") {
+    resultado = Number(parametroNum1) / Number(parametroNum2);
+    res.send(resultado.toString());
+  } else if (parametroNum1 && parametroNum2 && parametroNum2 === "0") {
+    resultado = "Não é possível dividir por zero.";
+    res.send(resultado.toString());
+  }
+});
+
+app.listen(porta);
