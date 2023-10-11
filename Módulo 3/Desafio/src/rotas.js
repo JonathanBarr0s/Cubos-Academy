9e5583b8a52953ced185f3a5eb575fed4fcb2cf2
Jonathan Barros <132490863+JonathanBarr0s@users.noreply.github.com>
2023-10-11 12:37:50 -0300

    Att

--- Módulo 3/Desafio/src/rotas.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const controlador = require("./controlador/controladores");
-
-const rotas = express();
-
-rotas.post("/usuario", controlador.cadastrarUsuario);
-
-module.exports = rotas;
